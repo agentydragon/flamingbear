c761cb2fc2f408c01a36687b82ab805a59fe908c
Michal Pokorný <pok@rny.cz>
2013-03-30 22:42:07 +0100

    better machine detection

--- lib/boot.sh
+++ lib/boot.sh
@@ -12,3 +12,25 @@ find_modules
 [ -n "$MODULES" ] || die "No modules defined."
 
 MAIN_PID=$$
+
+WEEK=$((7 * 24 * 60 * 60))
+END=$(date +%s)
+START=$(( $END - 24 * 60 * 60 ))
+
+# Setup default values. Command : does nothing, sucessfully. It is there to 
+# safely evaluate variable substitutions. Don't worry that your editors sees 
+# it as a comment. It is a comment. With sideeffects.
+
+: ${STEP:=120}
+: ${UN:=$(( 2 * $STEP ))}
+
+: ${KEEP:=$(( $WEEK / $STEP ))}
+
+: ${GEOMETRY:="--width 712"}
+: ${FONTS:="--font TITLE:12: --font AXIS:8:	--font LEGEND:10: --font UNIT:8:"}
+: ${STYLE:="$FONTS --slope-mode"}
+
+: ${RRD_DIR:=rrd}
+: ${LOCK_DIR:=locks}
+: ${GRAPH_DIR:=graphs}
+: ${LOG_FILE:=watchman.log}
--- lib/functions.sh
+++ lib/functions.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-#####################
-WEEK=$(( 7 * 24 * 60 * 60 ))
-END=$(date +%s)
-START=$(( $END - 24 * 60 * 60 ))
-#START=$(( $END - 10 * 60 ))
-
-# Setup default values. Command : does nothing, sucessfully. It is there to 
-# safely evaluate variable substitutions. Don't worry that your editors sees 
-# it as a comment. It is a comment. With sideeffects.
-
-: ${STEP:=120}
-: ${UN:=$(( 2 * $STEP ))}
-
-: ${KEEP:=$(( $WEEK / $STEP ))}
-
-: ${GEOMETRY:="--width 712"}
-: ${FONTS:="--font TITLE:12: --font AXIS:8:	--font LEGEND:10: --font UNIT:8:"}
-: ${STYLE:="$FONTS --slope-mode"}
-
-: ${GRAPH_DIR:=graphs}
-#####################
-
 function log() {
 	echo "$(date +%Y-%m-%d\ %H:%M) $@" >> $LOG_FILE
 }
@@ -64,7 +42,7 @@ function find_modules() {
 
 function load_machine() {
 	MACHINE=$(echo "$1" | sed -e 's/^ *//' -e 's/ *$//')
-	(echo "$MACHINES" | grep -- "$MACHINE" > /dev/null) || die "No such machine known: $MACHINE"
+	(echo " $MACHINES " | grep -- " $MACHINE " > /dev/null) || die "No such machine known: $MACHINE"
 	source "etc/machines/${MACHINE}.conf"
 	debug "Machine $MACHINE loaded."
 }
